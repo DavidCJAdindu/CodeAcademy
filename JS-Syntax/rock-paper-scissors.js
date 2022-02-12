61758f2efd233c479abc895ec8ec5eef3d8e46fa
David Adindu <david.adindu@infoproj.com>
2022-02-12 23:49:54 +0000

    rock, paper, scissors

--- JS-Syntax/rock-paper-scissors.js
+++ JS-Syntax/rock-paper-scissors.js
@@ -20,3 +20,105 @@ Our code will break the game into four parts:
 
 
 */
+
+
+
+// function that takes user input "rock, paper or scissors"
+const getUserChoice  = (userInput) => {
+
+userInput = userInput.toLowerCase();
+
+if (userInput !== 'rock' && userInput !== 'paper' && userInput !== 'scissors') {
+    return 'NOT ROCK, PAPER OR SCISSORS!';
+    } else {
+    return userInput;
+    }
+
+};
+
+
+// function that returns a rock paper or scissors value 
+const getComputerChoice = () => {
+
+let randNum = Math.floor(Math.random()*3);
+
+// console.log(randNum);
+
+if (randNum === 0) {
+    return 'rock';
+    } else if (randNum === 1) {
+    return 'paper';
+    } else {
+    return 'scissors';
+    }
+
+}
+
+
+// function that determines a winner 
+function determineWinner(userChoice, computerChoice) {
+
+    if(userChoice === computerChoice){
+        return ("The game is a tie.");
+    }
+
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+
+}
+
+
+
+// game functionality 
+const playGame = () => {
+
+// enter rock,paper or scissors into arguement of getUserChoice 
+const userChoice = getUserChoice('rock');
+const computerChoice = getComputerChoice();
+
+console.log('You threw: ' + userChoice);
+console.log('The computer threw: ' + computerChoice);
+console.log(determineWinner(userChoice, computerChoice));
+
+};
+
+
+// Call the playGame() function to start the game 
+playGame();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
